02018f6659ba2305453a4b51ab5e1f0162129d92
Brendan Aguiar <50648429+brendankaguiar@users.noreply.github.com>
2021-06-16 11:05:23 -0700

    Update UART_Practice.ino

--- UART_Practice.ino
+++ UART_Practice.ino
@@ -3,7 +3,7 @@
  * 
  * UART Setup for Arduino Due
  * By Brendan Aguiar
- * Description: This program uses Due Register assignments for communication between the keyboard and serial monitor.
+ * Description: This program uses Arduino Due register assignments for communication between the keyboard and serial monitor.
  */
 //UART Register Assignments
 volatile unsigned long* UART_CR = (unsigned long*)0x400E0800; //Control Register (Write-Only)
@@ -12,45 +12,39 @@ volatile unsigned long* UART_SR = (unsigned long*)0x400E0814; //Status Register 
 volatile unsigned long* UART_RHR = (unsigned long*)0x400E0818; //Receiver Holding Register (Read-Only)
 volatile unsigned long* UART_THR = (unsigned long*)0x400E081C; //Transmitter Holding Register (Write-Only)
 volatile unsigned long* UART_BRGR = (unsigned long*)0x400E0820; //Baud Rate Generator Register (Read-Write)
-unsigned long MCK = 84000000; //Hz
-unsigned char hex_msb;
-unsigned char hex_lsb;
+unsigned long MCK = 84000000; //Clock speed in Hz
 
-void setup() {
-  // put your setup code here, to run once:
-  UART_init(9600);
+
+void setup() 
+{
+  UART_init(9600); //initialize UART
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
   unsigned char msg;
-  while (UART_Available() == 0);
   msg = read_char();
   print_char(msg);
-  //print_char('\n');
+  print_char('\n');
   
 }
 
 void UART_init(unsigned long CD)
 {
-  unsigned int baud_rate = MCK / (16 * CD);
-  *UART_CR = 0x00000050; //Enable Receive and Transmit
-  *UART_MR = 0x00000800; //No Parity, Normal Mode
-  *UART_BRGR = baud_rate;
-}
-
-unsigned char UART_Available() // Read USART0 RDA status bit and return non-zero true if set
-{
-  return *UART_SR & 0x00000001;
+  unsigned int baud_rate = MCK / (16 * CD); //( master clk / (16 * clk divisions));
+  *UART_CR = 0x00000050; //enable receive and transmit
+  *UART_MR = 0x00000800; //no parity, normal mode
+  *UART_BRGR = baud_rate;//set baud rate
 }
 
 unsigned char read_char()
 {
-  return *UART_RHR;
+  while ((*UART_SR & 0x00000001) == 0); //wait for unread character to receive
+  return *UART_RHR; //read from Receiver Holding Register
 }
 
 void print_char(unsigned char data)
 {
-  while ((*UART_SR & 0x00000002) == 0);
-  *UART_THR = data;
+  while ((*UART_SR & 0x00000002) == 0); //wait for unprinted character to transmit
+  *UART_THR = data; // write to Transmitter Holding Register
 }
